c8c9d375e16ce653e8678a891de70ca12c69fb79
Yo <mario.lr@kaist.ac.kr>
2020-04-24 00:10:07 +0200

    Fix curl lib installation issue

--- 0_install_r_libs.sh
+++ 0_install_r_libs.sh
@@ -10,8 +10,13 @@
 # Exit immediately if a command exits with a error status.
 set -e
 
-# Install Ubuntu dependancies
-apt-get install -y libcurl4-openssl-dev
+# Install `curl` library, pre-requirement for R packages.
+# NOTE: It is not useful to install with package manager as:
+#       apt-get install -y libcurl4-openssl-dev
+# This is a known issue for R 3.4 on Ubuntu 18
+# See  https://stackoverflow.com/a/60878166 for details.
+wget https://cloud.r-project.org/src/contrib/curl_4.3.tar.gz
+R CMD INSTALL --configure-vars='LIB_DIR=/usr/lib/x86_64-linux-gnu/pkgconfig' curl_4.3.tar.gz
 
 # Create script to install R libs.
 # NOTE: Fill free to add any lib name to `required_packages` list below, as needed.
@@ -19,7 +24,7 @@ echo "print('INFO - Installing required R libraries'); \
 required_packages <- c('dplyr', 'sparklyr'); \
 installed_packages <- installed.packages()[,1]; \
 to_install <- setdiff(required_packages, installed_packages); \
-if (length(to_install) > 0) { install.packages(to_install, repos = 'https://cloud.r-project.org', quiet = TRUE); \
+if (length(to_install) > 0) { install.packages(to_install, repos = 'https://cloud.r-project.org', quiet = FALSE); \
 print('INFO - Succesfully installed required R libraries') } else { \
 print('INFO - All required R libraries are already installed')}" >> install.R
 
